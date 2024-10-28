0389b85fdc5b8d7c43293dc6741ad82e93e794c2
Condy <132919366+yu03-dev@users.noreply.github.com>
2024-10-29 03:34:01 +0900

    レイアウト修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { SideMenu } from "@/components/common/SideMenu";
 import type React from "react";
 
 export const metadata: Metadata = {
@@ -12,18 +13,14 @@ export default function RootLayout({
 }: { children: React.ReactNode }) {
 	return (
 		<html lang="ja">
-			<body>{children}</body>
-			{/*<main className="flex-1 flex justify-center items-center p-5">*/}
-			{/*	<div className="bg-white rounded-xl shadow-lg p-5 max-w-xs w-full text-center">*/}
-			{/*		<div className="bg-black bg-opacity-60 text-white p-3 rounded-b-xl h-36 overflow-y-scroll">*/}
-			{/*			<h3 className="text-lg font-bold mb-2">{children.name}</h3>*/}
-			{/*			<p className="text-sm">{children.comment}</p>*/}
-			{/*		</div>*/}
-			{/*		<button className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 hover:bg-blue-700">*/}
-			{/*			{children.button}*/}
-			{/*		</button>*/}
-			{/*	</div>*/}
-			{/*</main>*/}
+			<body className="flex h-screen overflow-hidden">
+				<SideMenu />
+				<main className="ml-52 flex-1 p-5 flex justify-center h-full">
+					<div className="rounded-xl shadow-lg text-center max-w-md w-full mx-auto">
+						{children}
+					</div>
+				</main>
+			</body>
 		</html>
 	);
 }
--- components/common/SideMenu.tsx
+++ components/common/SideMenu.tsx
@@ -2,20 +2,18 @@ import React from "react";
 
 export function SideMenu() {
 	return (
-		<div className="flex min-h-screen">
-			<aside className="bg-gray-800 text-white p-5 w-52 fixed top-0 left-0 h-full flex flex-col">
-				<h2 className="mb-4">MENU</h2>
-				<ul>
-					<li>
-						場所
-						<ul className="ml-4">
-							<li>賃貸</li>
-							<li>えーごはん</li>
-							<li>授業・学内マップ</li>
-						</ul>
-					</li>
-				</ul>
-			</aside>
-		</div>
+		<aside className="flex flex-col fixed top-0 left-0 w-52 h-full p-5 gap-4 bg-gray-800 text-white">
+			<h2>MENU</h2>
+			<ul>
+				<li>
+					場所
+					<ul className="ml-4">
+						<li>賃貸</li>
+						<li>えーごはん</li>
+						<li>授業・学内マップ</li>
+					</ul>
+				</li>
+			</ul>
+		</aside>
 	);
 }
