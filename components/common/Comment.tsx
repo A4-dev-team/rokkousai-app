a475c3986fbbd4bc0a4dc349c5e9639350495201
Condy <132919366+yu03-dev@users.noreply.github.com>
2024-10-29 03:01:17 +0900

    コンポーネントの修正

--- components/common/Comment.tsx
+++ components/common/Comment.tsx
@@ -1,17 +1,13 @@
-import "././globals.css";
 import React from "react";
 
-interface CommentProps {
-  comment: string;
-}
-export default function Comment({ comment }: CommentProps) {
-  <html lang="ja">
-    <main className="flex-1 flex justify-center items-center p-5">
-      <div className="bg-white rounded-xl shadow-lg p-5 max-w-xs w-full text-center">
-        <div className="bg-black bg-opacity-60 text-white p-3 rounded-b-xl h-36 overflow-y-scroll">
-          <p className="text-sm">{comment}</p>
-        </div>
-      </div>
-    </main>
-  </html>;
+export function Comment({ comment }: { comment: string }) {
+	return (
+		<main className="flex-1 flex justify-center items-center p-5">
+			<div className="bg-white rounded-xl shadow-lg p-5 max-w-xs w-full text-center">
+				<div className="bg-black bg-opacity-60 text-white p-3 rounded-b-xl h-36 overflow-y-scroll">
+					<p className="text-sm">{comment}</p>
+				</div>
+			</div>
+		</main>
+	);
 }
