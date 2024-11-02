e154e49ea58b45c671a130a523d510659760f72d
Condy <132919366+yu03-dev@users.noreply.github.com>
2024-11-03 05:18:36 +0900

    整理

--- components/common/FormInput.tsx
+++ components/common/FormInput.tsx
@@ -1,8 +1,8 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 
+// NOTE: 親は必ずClient Component
 export function FormInput({
 	placeholder,
 	onSubmit,
