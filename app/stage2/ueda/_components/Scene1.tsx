e26dcfdc04cbe646609ff2a18a37889426d49014
RYOTA <ryota0610ryota@gmail.com>
2024-11-06 17:45:44 +0900

    stage2の完成

--- app/stage2/ueda/_components/Scene1.tsx
+++ app/stage2/ueda/_components/Scene1.tsx
@@ -1,17 +1,73 @@
 "use client";
-import { OneLineTypeWriter } from "@/components/common/OneLineTypeWriter";
+import {
+	type DialogueProps,
+	DialogueController,
+} from "@/components/common/DialogueController";
 import { ScrollableChatWindow } from "@/components/common/ScrollableChatWindow";
 
-const word =
-	"ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。ここに長い文章を追加して、スクロールできるようにします。";
+export function Scene1() {
+	const dialogues: DialogueProps[] = [
+		{
+			type: "text",
+			name: "主人公",
+			text: "すみません、少しお話を伺ってもよろしいでしょうか？",
+		},
+		{
+			type: "text",
+			name: "上田",
+			text: "え？あ、いいですよ。何かご用ですか？",
+		},
+		{
+			type: "text",
+			name: "主人公",
+			text: "実は、ここで以前アルバイトしていた大学生について少し調べているんです。もしかして、上田さんも何か覚えていることはありますか？",
+		},
+		{
+			type: "text",
+			name: "上田",
+			text: "ああ、あの人のことですね…。ちょっと変わった人でしたけど、悪い人じゃなかったですよ。あんまり深くは話さなかったけど。",
+		},
+		{
+			type: "text",
+			name: "主人公",
+			text: "何か印象に残っていることなどがあれば、教えていただけますか？",
+		},
+		{
+			type: "text",
+			name: "上田",
+			text: "そうですねえ…そういえば、たまたま帰りがけに会って、空を見上げてたので『月が好きなの？』って聞いたら、『天体が好きですねー』って答えてましたね。",
+		},
+		{
+			type: "text",
+			name: "主人公",
+			text: "天体が好き…ですか。なんだか意外です。彼が天体に興味を持っているなんて、他の方からは聞いたことがなかったので。",
+		},
+		{
+			type: "text",
+			name: "上田",
+			text: "ええ、そうですよね。こっちも『そうなんだ』くらいしか反応しなかったんですけど…",
+		},
+		{
+			type: "text",
+			name: "主人公",
+			text: "ありがとうございます、上田さん。もしかしたら、天体に関することが彼にとって大事な要素だったのかもしれませんね。",
+		},
+		{
+			type: "text",
+			name: "上田",
+			text: "そうかもしれませんね…。でも、それくらいしか覚えていなくて、お役に立てなくてすみません。」",
+		},
+		{
+			type: "text",
+			name: "主人公",
+			text: "いえ、十分役立っています。小さなことでもヒントになりますから。",
+		},
+	];
 
-export function Scene1(): JSX.Element {
 	return (
 		<div className="h-full flex flex-col justify-end">
 			<ScrollableChatWindow>
-				<div className="text-white font-medium">
-					<OneLineTypeWriter word={word} />
-				</div>
+				<DialogueController dialogues={dialogues} />
 			</ScrollableChatWindow>
 		</div>
 	);
