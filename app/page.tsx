32aa92c8b0718b67bde19e02763860a06e900bfc
Shota Mizusaki <nrxg129@gmail.com>
2024-11-09 02:19:35 +0900

    barとhomeの実装

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,30 @@ import { IoInformationCircleOutline } from "react-icons/io5";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen bg-cover bg-center bg-[url('/background.png')]">
+    <div className="flex justify-center items-center h-screen bg-cover bg-top bg-[url('/home-background.png')]">
       <div className="absolute top-0 left-0 w-12 h-12 bg-white rounded-lg shadow-lg"></div>
-			<div className="flex flex-col items-center max-w-lg bg-white bg-opacity-80 p-6 rounded-lg shadow-lg fixed">
-        <h1 className="text-2xl font-bold mb-2">Thank you for coming !!!</h1>
-				<div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 my-3" role="alert">
-					<p className="font-bold text-left flex items-center space-x-1"><IoInformationCircleOutline />最初にすること</p>
-					<p className="text-sm text-left">オープニング動画を見て犯人のメッセージを確認しよう！</p>
-				</div>
-				<div>
-					{/* YouTube動画埋め込み */}
-					<iframe 
-						width="100%" 
-						src="https://www.youtube.com/embed/mwjFAGeo7v4?si=JjequE6UhrldwNbI" 
-						title="YouTube video player" 
-						frameBorder="0" 
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-						referrerPolicy="strict-origin-when-cross-origin" 
-						allowFullScreen>
-					</iframe>
-					</div>
-				</div>
+			<div className="flex flex-col items-center bg-white bg-opacity-80 p-8 sm:p-12 rounded-lg shadow-lg fixed w-[90%] sm:w-[90%] md:w-[80%] lg:w-[70%] h-1/2 lg:h-[70%]" >
+      <h1 className="text-3xl sm:text-4xl font-bold mb-4">Thank you for coming !!!</h1>
+      <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-6 py-4 my-6 w-full" role="alert">
+        <p className="font-bold text-left flex items-center space-x-1">
+          <IoInformationCircleOutline /> 最初にすること
+        </p>
+        <p className="text-base sm:text-lg text-left">オープニング動画を見て犯人のメッセージを確認しよう!</p>
+      </div>
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="aspect-w-16 aspect-h-9 w-full h-full sm:w-[90%] md:w-[80%] lg:w-[70%] flex justify-center items-center">
+          <iframe 
+            src="https://www.youtube.com/embed/mwjFAGeo7v4?si=JjequE6UhrldwNbI" 
+            title="YouTube video player" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+            referrerPolicy="strict-origin-when-cross-origin" 
+            allowFullScreen
+            className="w-full h-full"
+          />
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
