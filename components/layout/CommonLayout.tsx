3855e12316a49ce37f7589d1864c6786ec37f631
RYOTA <ryota0610ryota@gmail.com>
2024-11-08 17:14:17 +0900

    隙間の削除

--- components/layout/CommonLayout.tsx
+++ components/layout/CommonLayout.tsx
@@ -16,7 +16,7 @@ export function CommonLayout(props: CommonLayoutProps) {
 
 			{/* メインコンテンツを画面全体に表示 */}
 			<div className="flex justify-center h-full flex-1 ">
-				<div className="max-w-md w-full shadow-lg text-center">{children}</div>
+				<div className="w-full shadow-lg text-center">{children}</div>
 			</div>
 		</div>
 	);
