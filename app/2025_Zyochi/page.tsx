c11305aa06606d8b190f2ebead8d9fa471172083
Shota Mizusaki <nrxg129@gmail.com>
2024-11-09 07:23:05 +0900

    背景などを整えた

--- app/2025_Zyochi/page.tsx
+++ app/2025_Zyochi/page.tsx
@@ -1,6 +1,7 @@
 export default function Page() {
 	return (
     <div className="flex justify-center items-center min-h-screen overflow-y-auto">
+      <div className="fixed top-1 left-1 w-12 h-12 bg-gray-800 z-20"></div>
       <img
         src="/2025_zyochi.png"
         alt="背景画像"
