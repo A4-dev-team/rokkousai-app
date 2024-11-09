7d634d08c90da5980d5318ff1510715c26cded06
Shota Mizusaki <nrxg129@gmail.com>
2024-11-10 02:16:01 +0900

    ModalWindowの追加

--- app/stage3/roi/page.tsx
+++ app/stage3/roi/page.tsx
@@ -4,8 +4,12 @@ import {
 	DialogueController,
 } from "@/components/common/DialogueController";
 import { ScrollableChatWindow } from "@/components/common/ScrollableChatWindow";
+import { useState } from "react";
+import ImageModal from "@/components/common/ImageModal";
 
 export default function Page() {
+	const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
 	const dialogues: DialogueProps[] = [
 		{
 			type: "text",
@@ -27,6 +31,7 @@ export default function Page() {
 			name: "ロイ先生",
 			text: "ほう、紙の切れ端ね。これのことかな？（ポケットから切れ端を取り出す）※拡大画像がうまく表示できない可能性があります。その場合はLINEで「紙切れ1」と送ってください。",
 			imageUrls: ["/67.png"],
+			onOpenModal: (url: string) => setSelectedImage(url),
 		},
 		{
 			type: "text",
@@ -38,6 +43,7 @@ export default function Page() {
 			name: "ロイ先生",
 			text: "ああ、そういえば…君のような学生が来たら、渡せと言われたものがあと2枚...（別の紙を取り出す）※拡大画像がうまく表示できない可能性があります。その場合はLINEで「別の紙切れ」と送ってください。",
 			imageUrls: ["/65.png", "/81.png", "/99.png"],
+			onOpenModal: (url: string) => setSelectedImage(url),
 		},
 		{
 			type: "text",
@@ -46,11 +52,22 @@ export default function Page() {
 		},
 	];
 
+	const openModal = (url: string) => {
+    setSelectedImage(url);
+  };
+
+	const closeModal = () => {
+    setSelectedImage(null);
+  };
+
 	return (
 		<div className="h-full flex flex-col justify-end">
 			<ScrollableChatWindow>
-				<DialogueController dialogues={dialogues} />
+				<DialogueController dialogues={dialogues} onOpenImageModal={openModal}/>
 			</ScrollableChatWindow>
+			{selectedImage && (
+        <ImageModal selectedImage={selectedImage} closeModal={closeModal} />
+      )}
 		</div>
 	);
 }
--- app/stage3/tazaki/page.tsx
+++ app/stage3/tazaki/page.tsx
@@ -4,8 +4,12 @@ import {
 	DialogueController,
 } from "@/components/common/DialogueController";
 import { ScrollableChatWindow } from "@/components/common/ScrollableChatWindow";
+import { useState } from "react";
+import ImageModal from "@/components/common/ImageModal";
 
 export default function Page() {
+	const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
 	const dialogues: DialogueProps[] = [
 		{
 			type: "text",
@@ -27,6 +31,7 @@ export default function Page() {
 			name: "田崎先生",
 			text: "なるほど、これのことかな。（机から紙の切れ端を取り出して渡す）※拡大画像がうまく表示できない可能性があります。その場合はLINEで「紙切れ4」と送ってください。",
 			imageUrls: ["/68.png"],
+			onOpenModal: (url: string) => setSelectedImage(url),
 		},
 		{
 			type: "text",
@@ -35,11 +40,22 @@ export default function Page() {
 		},
 	];
 
+	const openModal = (url: string) => {
+    setSelectedImage(url);
+  };
+
+	const closeModal = () => {
+    setSelectedImage(null);
+  };
+
 	return (
 		<div className="h-full flex flex-col justify-end">
 			<ScrollableChatWindow>
-				<DialogueController dialogues={dialogues} />
+				<DialogueController dialogues={dialogues} onOpenImageModal={openModal}/>
 			</ScrollableChatWindow>
+			{selectedImage && (
+        <ImageModal selectedImage={selectedImage} closeModal={closeModal} />
+      )}
 		</div>
 	);
 }
