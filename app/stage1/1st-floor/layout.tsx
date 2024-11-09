2d476d6f4e5ff3108d4f273aa822ad3fad606cec
Shota Mizusaki <nrxg129@gmail.com>
2024-11-09 10:10:04 +0900

    z後ろにしてみる

--- app/stage1/1st-floor/layout.tsx
+++ app/stage1/1st-floor/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="h-full rounded-xl relative bg-cover bg-center bg-[url('/rental-ooya.png')]">
+		<div className="h-full rounded-xl relative bg-cover bg-center bg-[url('/rental-ooya.png')] z-0">
 			{children}
 		</div>
 	);
