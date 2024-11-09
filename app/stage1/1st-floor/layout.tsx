efb20a0180b674d717e0b0e9f7c79217c32641c8
Shota Mizusaki <nrxg129@gmail.com>
2024-11-09 10:14:57 +0900

    完全に分離

--- app/stage1/1st-floor/layout.tsx
+++ app/stage1/1st-floor/layout.tsx
@@ -1,7 +1,16 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="h-full rounded-xl relative bg-cover bg-center bg-[url('/rental-ooya.png')] z-0">
-			{children}
+		<div className="h-full rounded-xl relative">
+			{/* 背景画像レイヤー */}
+			<div 
+				className="fixed inset-0 bg-cover bg-center bg-[url('/rental-ooya.png')] rounded-xl"
+				style={{ zIndex: -1 }}
+			/>
+			
+			{/* コンテンツレイヤー */}
+			<div className="relative h-full">
+				{children}
+			</div>
 		</div>
 	);
 }
