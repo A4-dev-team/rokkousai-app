cec9328aa769b00d015934208222993be16de389
Shota Mizusaki <nrxg129@gmail.com>
2024-11-10 08:14:19 +0900

    画像の微調整

--- app/stage1/3rd-floor/layout.tsx
+++ app/stage1/3rd-floor/layout.tsx
@@ -1,6 +1,6 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-    <div className="h-full rounded-xl relative bg-cover bg-center">
+    <div className="h-full rounded-xl relative bg-cover bg-center bg-[url('/rental-yasui.png')]">
       {children}
     </div>
   );
